018d85cca3a6241d7facead3e88acb1c41fdee70
Luba Kruglaya <lubchik.14@gmail.com>
2019-12-10 16:55:30 +0200

    Changed period of rate

--- src/main/java/helloworld/HelloWorldMain.java
+++ src/main/java/helloworld/HelloWorldMain.java
@@ -11,10 +11,10 @@ public class HelloWorldMain {
 
         final Runnable helloPrinter = () -> System.out.print("Hello ");
         final Runnable worldPrinter = () -> System.out.print("World ");
-        final Runnable enterPrinter = () -> System.out.print("!\n");
+//        final Runnable enterPrinter = () -> System.out.print("!\n");
 
-        ScheduledFuture<?> hello = scheduler.scheduleAtFixedRate(helloPrinter, 0, 1, TimeUnit.SECONDS);
-        ScheduledFuture<?> world = scheduler.scheduleAtFixedRate(worldPrinter, 0, 1, TimeUnit.SECONDS);
+        ScheduledFuture<?> hello = scheduler.scheduleAtFixedRate(helloPrinter, 0, 10, TimeUnit.SECONDS);
+        ScheduledFuture<?> world = scheduler.scheduleAtFixedRate(worldPrinter, 0, 10, TimeUnit.SECONDS);
 //        ScheduledFuture<?> enter = scheduler.scheduleAtFixedRate(enterPrinter, 1, 1, TimeUnit.SECONDS);
 
         scheduler.schedule(() -> hello.cancel(true), 60, TimeUnit.SECONDS);
